7fd80fde83fe3c411144de20b11986346834dc6f
mike mcginty <mach327@gmail.com>
2018-03-10 14:23:23 -0500

    symlinks for latest price data, and add the paper's scraper

--- scripts/price_indexes.sh
+++ scripts/price_indexes.sh
@@ -8,13 +8,15 @@ coindesk(){
 		exchange="bpi"
 	fi
 	output="csv"  #also json
+	outname="coindesk_${exchange}_${startdate}-${enddate}_${close_or_ohlc}.${output}"
 	curl "https://api.coindesk.com/charts/data?output=${output}&data=${close_or_ohlc}&index=USD&startdate=${startdate}&enddate=${enddate}&exchanges=${bpi}&dev=1" \
 		-H 'Host: api.coindesk.com'  \
 		-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0' \
 		-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' \
 		--compressed \
 		-H 'Referer: https://www.coindesk.com/price/' \
-		-o data/coindesk_${exchange}_${startdate}-${enddate}_${close_or_ohlc}.${output}
+		-o data/"${outname}"
+	ln -sr data/"${outname}" data/"${exchange}"pricedata."${output}"
 
 }
 
