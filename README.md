c01a3f66f6adc3e74a8294b10e1ff2087bb93835
mike mcginty <mach327@gmail.com>
2018-03-10 13:51:29 -0500

    .

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # metacrypto
+
+Mike:
+	output timestamped sentiment sums per post
+	price index history
+
+https://www.coindesk.com/price/
+	export csv
+
+
+
---
+++ scripts/price_indexes.sh
@@ -0,0 +1,18 @@
+coindesk(){
+	close_or_ohlc="ohlc" #or ohlc or close
+	#startdate="2010-07-17"
+	startdate="2008-03-01"
+	enddate=$(date "+%Y-%m-%d")
+	exchange="bpi" #bpi, or eth, or ltc
+	output="csv"  #also json
+	curl "https://api.coindesk.com/charts/data?output=${output}&data=${close_or_ohlc}&index=USD&startdate=${startdate}&enddate=${enddate}&exchanges=${bpi}&dev=1" \
+		-H 'Host: api.coindesk.com'  \
+		-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0' \
+		-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' \
+		--compressed \
+		-H 'Referer: https://www.coindesk.com/price/' \
+		-o data/coindesk_${exchange}_${startdate}-${enddate}_${close_or_ohlc}.${output}
+
+}
+
+coindesk $@
